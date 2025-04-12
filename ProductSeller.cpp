ee8112941f89e4c6deeabf220275556b81dd076e
Dohee Kwon <rnsjthfql@gmail.com>
2025-04-12 16:43:44 +0900

    수정

--- ProductSeller.cpp
+++ ProductSeller.cpp
@@ -12,16 +12,16 @@ void ProductSeller::ShowMenu()
 
 void ProductSeller::ShowProductMenu()
 {
-	cout << "======= 상품 메뉴 =======" << endl;
-	cout << "  상처약" << endl;
-	cout << "  잠깨는약" << endl;
-	cout << "  기력치료제" << endl;
-	cout << "  화상치료제" << endl;
-	cout << "  기력의조각" << endl;
-	cout << "  고급상처약" << endl;
-	cout << "  벌레회피스프레이" << endl;
-	cout << "  실버스프레이" << endl;
-	cout << "  몬스터볼" << endl;
-	cout << "  슈퍼볼" << endl;
-	cout << "=========================" << endl;
+	cout << "========== 상품 메뉴 ==========" << endl;
+	cout << "  상처약" << setw(19) << "300원" << endl;
+	cout << "  잠깨는약" << setw(17) << "300원" << endl;
+	cout << "  기력치료제" << setw(15) << "500원" << endl;
+	cout << "  화상치료제" << setw(15) << "300원" << endl;
+	cout << "  기력의조각" << setw(15) << "1000원" << endl;
+	cout << "  고급상처약" << setw(15) << "1000원" << endl;
+	cout << "  벌레회피스프레이" << setw(9) << "300원" << endl;
+	cout << "  실버스프레이" << setw(13) << "400원" << endl;
+	cout << "  몬스터볼" << setw(17) << "300원" << endl;
+	cout << "  슈퍼볼" << setw(19) << "1000원" << endl;
+	cout << "===============================" << endl;
 }
--- ProductSeller.h
+++ ProductSeller.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Product.h"
+#include <iomanip>
 
 class ProductSeller
 {
